8058b686eff206a70a17100fdb3f3babd82d8ce8
Charlot DEDJINOU <dedjinoucharlotjoel@gmail.com>
2023-08-21 13:43:54 +0100

    add new application (listings) , define home route and make migration for the table Band

---
+++ merchex/listings/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from listings.models import Band
+
+def hello(request):
+    bands = Band.objects.all()
+    return HttpResponse(f"""
+        <h1>Hello Django !</h1>
+        <p>Mes groupes préférés sont :<p>
+        <ul>
+            <li>{bands[0].name}</li>
+            <li>{bands[1].name}</li>
+            <li>{bands[2].name}</li>
+        </ul>
+""")
