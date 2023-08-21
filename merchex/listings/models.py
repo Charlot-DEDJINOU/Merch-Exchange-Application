f1f3e60e446d8eccc624499fdc317e7e711c50a9
Charlot DEDJINOU <dedjinoucharlotjoel@gmail.com>
2023-08-21 19:33:07 +0100

    add template with styles

--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -2,3 +2,8 @@ from django.db import models
 
 class Band(models.Model) :
     name = models.fields.CharField(max_length=100)
+    genre = models.fields.CharField()
+    biography = models.fields.CharField()
+    year_formed = models.fields.IntegerField()
+    active = models.fields.BooleanField()
+    official_homepage = models.fields.URLField()
--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -4,12 +4,4 @@ from listings.models import Band
 
 def hello(request):
     bands = Band.objects.all()
-    return HttpResponse(f"""
-        <h1>Hello Django !</h1>
-        <p>Mes groupes préférés sont :<p>
-        <ul>
-            <li>{bands[0].name}</li>
-            <li>{bands[1].name}</li>
-            <li>{bands[2].name}</li>
-        </ul>
-""")
+    return render(request , 'listings/home.html' , {'bands' : bands})
